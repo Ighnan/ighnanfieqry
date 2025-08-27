67aa3ee087e84c52f5d25d6e10cecbee8ec7f129
Ighnan <154766071+Ighnan@users.noreply.github.com>
2025-08-27 12:07:47 +0700

    Create script.js
    Signed-off-by: Ighnan <154766071+Ighnan@users.noreply.github.com>

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+
+  function showSection() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+
+      if(sectionTop < triggerBottom) {
+        section.classList.add('show');
+      } else {
+        section.classList.remove('show');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', showSection);
+  showSection();
+});
